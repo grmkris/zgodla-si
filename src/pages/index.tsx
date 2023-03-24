068fd1d81e686134b9e16a6f810a0419be2878b9
matej2 <11059438+matej2@users.noreply.github.com>
2023-03-24 15:06:17 +0100

    Layout fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,21 +10,15 @@ const Home: NextPage = () => {
         <title>Document</title>
       </Head>
       <div className="font-body text-gray-600">
-        <div className="bg-orange-200 py-5 h-40 flex">
-          <div className="presledki sm:flex sm:items-center sm:justify-start align-bottom">
-            <img src="https://picsum.photos/50" className="mr-5 rounded-full" />
-            <h1 className="text-xl">Zgodovina na kratko</h1>
-          </div>
-        </div>
-        <div className="bg-orange-200 py-5 h-40 flex">
-          <div className="presledki">
-            <img src="https://picsum.photos/50" className="mr-5 rounded-full" />
+        <div className="bg-orange-200 pb-7">
+          <div className="presledki sm:flex sm:items-center sm:justify-start text-center pt-20">
+            <img src="https://picsum.photos/50" className="sm:mr-5 rounded-full inline" />
             <h1 className="text-xl">Zgodovina na kratko</h1>
           </div>
         </div>
         <div className="presledki">
           <main>
-            <div>
+            <div className="px-auto">
               <h2 className="podnaslov">Izpostavljeno</h2>
               <div className="inline-block">
                 <div className="kategorija sm:h-72 h-36"></div>
@@ -32,7 +26,7 @@ const Home: NextPage = () => {
             </div>
 
             <div>
-              <h2 className="podnaslov">Kategorije</h2>
+              <h2 className="podnaslov">Vsi videi</h2>
               <div className="inline-block">
                 <div className="kategorija h-36"></div>
                 <div className="kategorija h-36"></div>
