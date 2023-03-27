26d5bafb5a153ce3273d59645135ce512f628517
matej2 <11059438+matej2@users.noreply.github.com>
2023-03-27 12:04:34 +0200

    icons, font

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,27 +7,44 @@ const Home: NextPage = () => {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Document</title>
+        <title>Zgodovina na kratko</title>
+        <script src="https://kit.fontawesome.com/7ee5a6eba5.js" crossOrigin="anonymous"></script>
       </Head>
-      <div className="font-body text-gray-600">
-        <div className="bg-orange-200 pb-7">
-          <div className="presledki sm:flex sm:items-center sm:justify-start text-center pt-20">
-            <img src="https://picsum.photos/50" className="sm:mr-5 rounded-full inline" />
-            <h1 className="text-xl">Zgodovina na kratko</h1>
+      <div className="font-body text-gray-600 pb-10 inline-block">
+        <div className="bg-primary pb-7">
+          <div className="presledki sm:flex sm:justify-between pt-20">
+            <div className="sm:flex sm:items-center sm:justify-start text-center ">
+              <img src="https://picsum.photos/50" className="sm:mr-5 rounded-full inline" />
+              <h1 className="text-xl font-bold uppercase">Zgodovina na kratko</h1>
+            </div>
+            <div className="my-auto text-center">
+              <div className="povezava">
+                <a href="#"><i className="fa-brands fa-facebook fa-fw"></i></a>
+              </div>
+              <div className="povezava">
+                <a href="#"><i className="fa-brands fa-instagram fa-fw"></i></a>
+              </div>
+              <div className="povezava">
+                <a href="#"><i className="fa-brands fa-youtube fa-fw"></i></a>
+              </div>
+              
+              
+              
+            </div>
           </div>
         </div>
         <div className="presledki">
           <main>
             <div className="px-auto">
               <h2 className="podnaslov">Izpostavljeno</h2>
-              <div className="inline-block">
+              <div className="flex sm:justify-center">
                 <div className="kategorija sm:h-72 h-36"></div>
               </div>
             </div>
 
             <div>
               <h2 className="podnaslov">Vsi videi</h2>
-              <div className="inline-block">
+              <div className="float-left gap-5">
                 <div className="kategorija h-36"></div>
                 <div className="kategorija h-36"></div>
                 <div className="kategorija h-36"></div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,15 @@
 const config = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#ffd387',
+        secondary: '#ffa200',
+      },
+      fontFamily: {
+        body: ['Open Sans']
+      }
+    },
   },
   plugins: [],
 };
